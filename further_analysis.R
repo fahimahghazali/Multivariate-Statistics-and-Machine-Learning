21a5925b356e5ba8114f867c66971e1fb7bf0395
fahimahghazali <fahimahghazali@icloud.com>
2023-03-11 19:41:24 +0000

    update on further analysis for 3 vs 6 clusters on k-means

--- further_analysis.R
+++ further_analysis.R
@@ -1,16 +1,11 @@
 install.packages("palmerpenguins")
-remotes::install_github("allisonhorst/palmerpenguins")
 
+penguins = palmerpenguins::penguins
 head(penguins)
 X.penguins = data.frame(na.omit(penguins))
 X.penguins[, "species_sex"] <- rep(NA, nrow(X.penguins))
 
-levels(X.penguins$species_sex)[levels(X.penguins$species_sex)== "Adelie"
-                               & levels(X.penguins$species_sex)== "female"] <- "Adelie_f"
-
-X.penguins[X.penguins$species == "Adelie" & X.penguins$sex == "female", ]
 f = 0
-
 for (j in factor(X.penguins$species)){
   for (k in factor(X.penguins$sex)) {
     ind <- which(X.penguins$species == j & X.penguins$sex == k)
@@ -24,7 +19,7 @@ X.penguins$species_sex <- as.factor(X.penguins$species_sex)
 
 head(X.penguins)
 
-set.seed(543261)
+set.seed(432)
 Xpen = scale(X.penguins[,3:6], scale = T)
 kmc3 = kmeans(Xpen, 3)
 
@@ -32,14 +27,21 @@ kmc3 = kmeans(Xpen, 3)
 
 kmc6 = kmeans(Xpen, 6)
 par(mfrow=c(1,2), mai = c(0.8, 0.8, 0.5, 0.3))
-plot(Xpen[,3],Xpen[,4], col = kmc3$cluster, pch = as.integer(X.penguins$species), 
+plot(Xpen[,1],Xpen[,2], col = kmc3$cluster, pch = as.integer(X.penguins$species), 
      main = "K-means clusters vs known species", 
-     xlab = "flipper length/mm", ylab = "body mass/g")
+     xlab = "bill length/mm", ylab = "bill depth/mm")
+legend("bottomleft", legend = c(unique(X.penguins$species)), 
+       pch = as.integer(unique(X.penguins$species)), bty = "o", cex = 0.9)
+legend("bottomright", legend = c("Cluster 1", "Cluster 2", "Cluster 3"), 
+       col = as.integer(unique(kmc3$cluster)), bty = "o", cex = 0.9, lty = 1)
 
-plot(Xpen[,3],Xpen[,4], col = kmc6$cluster, pch = as.integer(X.penguins$species_sex), 
+plot(Xpen[,1],Xpen[,2], col = kmc6$cluster, pch = as.integer(X.penguins$species_sex), 
      main = "K-means clusters vs known species and sex", 
-     xlab = "flipper length/mm", ylab = "body mass/g")
-
+     xlab = "bill length/mm", ylab = "bill depth/mm")
+legend("bottomleft", legend = c(unique(X.penguins$species_sex)), 
+       pch = as.integer(unique(X.penguins$species_sex)), bty = "o", cex = 0.9)
+legend("bottomright", legend = c("Cluster 1", "Cluster 2", "Cluster 3", "Cluster 4", "Cluster 5", "Cluster 6"), 
+       col = as.integer(unique(kmc6$cluster)), bty = "o", cex = 0.9, lty = 1)
 
 addmargins(table(X.penguins$species, kmc3$cluster))
 addmargins(table(X.penguins$species_sex, kmc6$cluster))
@@ -55,4 +57,10 @@ for (k in 1:maxk) {
 par(mfrow=c(1,1))
 plot(1:maxk, bssvec, type="b", xlab="no of groups", ylab="Within/between Group SS", main="")
 points(1:maxk, wssvec, type="b", col=2, lty="dashed", pch=2)
-legend("right", c("Between SS (explained)", "Within SS (unexplained)"), col=c(1,2), pch=c(1,2), cex = 0.7)
+legend("right", c("Between SS (explained)", "Within SS (unexplained)"), col=c(1,2), pch=c(1,2))
+
+
+
+
+
+
